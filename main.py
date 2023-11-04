dc256143b16d25dc9c505d03864e4a81947ad464
Maximiliano Brignone <maximilianobrignone@gmail.com>
2023-11-04 18:25:23 -0300

    setup game loop

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import pygame
+import sys
 
+pygame.init()
+dar_blue = (44, 44, 127)
+
+icon = pygame.image.load('assets/icon.png')
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Py-Tetris")
+pygame.display.set_icon(icon)
+
+clock = pygame.time.Clock()
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	#drawing
+	screen.fill(dar_blue)
+	pygame.display.update()
+	clock.tick(60) # <-- using 60 FPS
